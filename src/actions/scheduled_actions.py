e696f60ba405f5e1e808a680a7bfa3fca80997e5
wahjian.lin@gmail.com <wahjian.lin@gmail.com>
2025-02-20 20:55:19 -0500

    [MODIFY] g-009 code clean up.

--- src/actions/scheduled_actions.py
+++ src/actions/scheduled_actions.py
@@ -4,20 +4,22 @@ from datetime import datetime
 from typing import Final
 
 from discord.ext import tasks
+from discord.types.channel import TextChannel
 from dotenv import load_dotenv
 
 from src.actions.data_actions import get_leaderboard_result
 
 load_dotenv()
 LEADERBOARD_CHANNEL: Final[int] = int(os.getenv('LEADERBOARD_CHANNEL'))
-RUN_TIMES_STR = os.getenv('SCHEDULE_LEADERBOARD_TIMES').split(",")
-RUN_TIMES = list(map(int, RUN_TIMES_STR))
+RUN_TIMES_STR: list[str] = os.getenv('SCHEDULE_LEADERBOARD_TIMES').split(",")
+RUN_TIMES: list[int] = list(map(int, RUN_TIMES_STR))
 
 
 @tasks.loop(minutes=10)
 async def schedule_leaderboard_caller(client):
     cur_hour: int = datetime.now().hour
-    message_channel = client.get_channel(LEADERBOARD_CHANNEL)
+    message_channel: TextChannel = client.get_channel(LEADERBOARD_CHANNEL)
+
     if message_channel:
         logging.info(f"schedule_leaderboard_caller triggered at {cur_hour} utc hour")
         if cur_hour in RUN_TIMES:
